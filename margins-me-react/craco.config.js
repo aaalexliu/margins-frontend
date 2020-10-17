c40dac5d0d28aca28efd49a22eebbfcd5a3ac274
alexliusq <uilxela7@gmail.com>
2020-10-17 10:33:24 +0800

    first attempt at customizing ant colors

---
+++ margins-me-react/craco.config.js
@@ -0,0 +1,26 @@
+const CracoLessPlugin = require('craco-less');
+const path = require('path');
+const fs = require('fs');
+
+const lessToJs = require('less-vars-to-js');
+const themeVariables = lessToJs(
+  fs.readFileSync(
+    path.join(__dirname, './config/ant-theme-vars.less'),
+    'utf-8')
+);
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: themeVariables,
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ]
+}
