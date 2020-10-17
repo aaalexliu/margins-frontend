502fdd5fd1e113095b90936dd7dabb07f62fbaba
alexliusq <uilxela7@gmail.com>
2020-10-17 15:01:15 +0800

    making index as minimal as possible, pages as the main entry point includes router

--- margins-me-react/src/index.tsx
+++ margins-me-react/src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
-import App from './App';
-import { BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import Pages from './pages';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App/>
-    </Router>
+    <Pages/>
   </React.StrictMode>,
   document.getElementById('root')
 );
