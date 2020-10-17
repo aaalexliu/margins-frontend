fac070224d1fb8c9cf5234fbe4ef64363ce71791
alexliusq <uilxela7@gmail.com>
2020-10-17 12:05:52 +0800

    set up menu item to test

--- margins-me-react/src/App.tsx
+++ margins-me-react/src/App.tsx
@@ -1,9 +1,21 @@
 import React, { FC } from 'react';
-import { Button } from 'antd';
+import {
+  Button,
+  Menu,
+} from 'antd';
+import MenuItem from 'antd/lib/menu/MenuItem';
 
 
 const App: FC = () => (
   <div className="App">
+    <Menu mode="horizontal">
+      <Menu.Item>
+        Sign Up
+      </Menu.Item>
+      <Menu.Item>
+        Login
+      </Menu.Item>
+    </Menu>
     <Button type="primary">Button</Button>
   </div>
 );
