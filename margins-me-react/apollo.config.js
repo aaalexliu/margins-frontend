8e54615f498886b28368434e4a5f809a13b279da
alexliusq <uilxela7@gmail.com>
2020-11-04 22:29:29 +0800

    updated apollo to point to graphql ec2. added dotenv to use env variable in config

--- margins-me-react/apollo.config.js
+++ margins-me-react/apollo.config.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
+// console.log(`Bearer ${process.env.GRAPHQL_JWT}`);
 module.exports = {
   client: {
-    service: 'margins-me-graphql@current',
-    includes: ['./src/client-schema.js']
+    service: {
+      name: 'margins-me-endpoint',
+      url: 'http://ec2-34-232-69-157.compute-1.amazonaws.com:8080/graphql',
+      // optional headers
+      headers: {
+        authorization: `Bearer ${process.env.GRAPHQL_JWT}`
+      },
+      // optional disable SSL validation check
+      skipSSLValidation: true,
+      includes: ['./src/client-schema.js']
+    }
   }
 }
