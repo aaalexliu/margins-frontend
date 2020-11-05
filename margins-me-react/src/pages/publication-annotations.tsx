7af918f27ba53eda8dc657d77ebe28e1de5fd0ee
alexliusq <uilxela7@gmail.com>
2020-11-05 10:30:31 +0800

    have query for publication annotations working

--- margins-me-react/src/pages/publication-annotations.tsx
+++ margins-me-react/src/pages/publication-annotations.tsx
@@ -1,11 +1,74 @@
 import React, { Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+import { Loading } from '../components';
+
+import {
+  PUBLICATION_AUTHOR_ANNOTATION_COUNT,
+  extractPublicationAuthorAnnotationCount
+} from '../utils/publication-author-annotation-count';
+import * as GetPublicationByPublicationIdTypes from './__generated__/GetPublicationByPublicationId';
+import { isNullOrUndefined } from 'util';
+
+export const GET_PUBLICATION_BY_PUBLICATION_ID = gql`
+  query GetPublicationByPublicationId($publicationId: String!) {
+    publicationByPublicationId(publicationId: $publicationId) {
+      ...PublicationAuthorAnnotationCount
+      bookByPublicationId {
+        bookTitle
+        bookType
+        description
+        imageUrl
+        languageCode
+        isbn13
+        publicationDate
+        publicationId
+        publisher
+      }
+    }
+  }
+  ${PUBLICATION_AUTHOR_ANNOTATION_COUNT}
+`;
+
+
 
 const PublicationAnnotations = () => {
 
   const { publicationId } = useParams();
+
+  const {
+    data,
+    loading,
+    error,
+    fetchMore
+  } = useQuery<
+    GetPublicationByPublicationIdTypes.GetPublicationByPublicationId,
+    GetPublicationByPublicationIdTypes.GetPublicationByPublicationIdVariables
+  >(
+    GET_PUBLICATION_BY_PUBLICATION_ID,
+    {
+      variables: {
+        publicationId
+      }
+    }
+  );
+
+  if (loading) return <Loading />;
+  if (error || !data) return <p>ERROR</p>;
+
+  const publicationData = data.publicationByPublicationId !== null ?
+    extractPublicationAuthorAnnotationCount(data.publicationByPublicationId)
+    : null;
+
   return(
-    <p>hello {publicationId}</p>
+    <Fragment>
+    {
+      publicationData !== null ?
+      <p>hello {publicationData.title}</p>
+      :
+      <p>boo</p>
+    }
+        </Fragment>
   )
 }
 
