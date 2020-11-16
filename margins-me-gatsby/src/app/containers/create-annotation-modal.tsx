166b5248192a37c9a4c17ac3154d6a070ecd0ad7
alexliusq <uilxela7@gmail.com>
2020-11-16 11:48:08 +0800

    added function to clean up data before submitting to graphql

--- margins-me-gatsby/src/app/containers/create-annotation-modal.tsx
+++ margins-me-gatsby/src/app/containers/create-annotation-modal.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Modal, Form } from 'antd';
 import { AnnotationForm } from '../components';
-import { CreateAnnotationDocument, GetAllAnnotationsForPublicationDocument } from '../__generated__/graphql-types'; 
+import { CreateAnnotationDocument, GetAllAnnotationsForPublicationDocument, CreateAnnotationInput } from '../__generated__/graphql-types'; 
 import { gql, useMutation } from '@apollo/client';
 import { generateObjectId } from '../utils/object-id';
 import { getAccountId } from '../utils/account-id';
@@ -13,13 +13,39 @@ interface CreateAnnotationModalProps {
   onCancel: () => void;
 }
 
+function createAnnotationInput(values: any, publicationId: string): CreateAnnotationInput {
+  const newAnnotation: any = {
+    annotationId: generateObjectId(),
+    accountId: getAccountId(),
+    publicationId,
+  }
+  console.log(values);
+
+  if (values.location != null) {
+    newAnnotation['highlightLocation'] = JSON.stringify(values.location);
+  }
+  if (values.highlightText != null && values.highlightText != '') {
+    newAnnotation['highlightText'] = values.highlightText;
+  }
+  if (values.noteText != null && values.noteText != '') {
+    newAnnotation['noteText'] = values.noteText;
+  }
+  if (values.color != null) {
+    newAnnotation['color'] = values.color
+  }
+  return {
+    annotation: newAnnotation
+  };
+}
+
 export const CreateAnnotationModal: React.FC<CreateAnnotationModalProps> = ({
   publicationId,
   visible,
   onCreate,
   onCancel,
 }) => {
-  const [form] = Form.useForm();
+  const [ form ] = Form.useForm();
+  const [ confirmLoading, setConfirmLoading ] = useState(false);
   const [ addAnnotation ] = useMutation(CreateAnnotationDocument, {
     update(cache, { data }) {
       const newAnnotationEdge = data?.createAnnotation?.annotationEdge;
@@ -45,43 +71,26 @@ export const CreateAnnotationModal: React.FC<CreateAnnotationModalProps> = ({
           }
         })
       }
-
-      // cache.modify({
-      //   fields: {
-      //     allAnnotations(existingAnnotationRefs = [], { DELETE }) {
-      //       console.log('existingannotationrefs');
-      //       console.log(existingAnnotationRefs);
-            // return DELETE;
-            // const newAnnotationEdge = data?.createAnnotation?.annotationEdge;
-            // if (newAnnotationEdge == null) return [...existingAnnotationRefs];
-
-            // const newAnnotationEdgeRef = cache.writeFragment({
-            //   data: newAnnotationEdge,
-            //   fragment: gql`
-            //     fragment NewAnnotationEdge on AnnotationEdge {
-            //       cursor
-            //       node {
-            //         ...AnnotationAll
-            //       }
-            //     }
-            //   `
-            // });
-            // return [...existingAnnotationRefs, newAnnotationEdgeRef];
-            // const newCommentRef = cache.writeFragment({
-            //   data: addComment,
-            //   fragment: gql`
-            //     fragment NewComment on Comment {
-            //       id
-            //       text
-            //     }
-            //   `
-            // });
-            // return [...existingCommentRefs, newCommentRef];
-          // }
-        // }
-      // });
     }
   });
+
+  const onModalOk = async () => {
+    setConfirmLoading(true);
+    try {
+      const values = await form.validateFields();
+      form.resetFields();
+      const { data } = await addAnnotation({
+        variables: {
+          inputAnnotation: createAnnotationInput(values, publicationId)
+        }
+      })
+      onCreate(data);
+    } catch(error) {
+      console.log('Validate Failed:', error);
+    } finally{
+      setConfirmLoading(false);
+    } 
+  }
   return (
     <Modal
       visible={visible}
@@ -89,32 +98,10 @@ export const CreateAnnotationModal: React.FC<CreateAnnotationModalProps> = ({
       okText="Create"
       cancelText="Cancel"
       onCancel={onCancel}
-      onOk={() => {
-        form
-          .validateFields()
-          .then(values => {
-            form.resetFields();
-            addAnnotation({
-              variables: {
-                inputAnnotation: {
-                  annotation: {
-                    annotationId: generateObjectId(),
-                    highlightLocation: JSON.stringify({kindleLocation: 1}),
-                    highlightText: 'testCreate1',
-                    accountId: getAccountId(),
-                    publicationId
-                  }
-                } 
-              }
-            })
-            onCreate(values);
-          })
-          .catch(info => {
-            console.log('Validate Failed:', info);
-          });
-      }}
+      confirmLoading={confirmLoading}
+      onOk={onModalOk}
     >
-      <AnnotationForm form={form} initialValues={{highlightColor: 'none'}}/>
+      <AnnotationForm form={form} initialValues={{color: 'none'}}/>
     </Modal>
   )
 }
