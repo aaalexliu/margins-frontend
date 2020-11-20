b9b8b628e5a147750ba19f77eee77245cba505e7
alexliusq <uilxela7@gmail.com>
2020-11-20 13:59:24 +0800

    exporting annototionpublicationcard

--- margins-me-gatsby/src/app/containers/index.ts
+++ margins-me-gatsby/src/app/containers/index.ts
@@ -1,5 +1,5 @@
 export { default as PublicationCard } from './publication-card';
-export { default as AnnotationCard } from './annotation-card';
+export { default as AnnotationCard, AnnotationContent, AnnotationAndPublicationCard } from './annotation-card';
 export { default as CreateAnnotationModal } from './create-annotation-modal';
 export { default as PublicationFormModal } from './publication-form-modal';
 export { default as TagCard } from './tag-card';
