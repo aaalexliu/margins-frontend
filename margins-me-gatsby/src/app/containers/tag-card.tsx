6ce21e7c9c6ebc9d204157ef0e2d3b208ddcc95e
alexliusq <uilxela7@gmail.com>
2020-11-22 11:47:45 +0800

    adding functionality to tag card to allow update and delete

--- margins-me-gatsby/src/app/containers/tag-card.tsx
+++ margins-me-gatsby/src/app/containers/tag-card.tsx
@@ -1,13 +1,17 @@
 import React, { Fragment, useState } from 'react';
 import { useMutation, useQuery, gql } from '@apollo/client';
-import { Card, Descriptions, Button, Typography } from 'antd';
+import { Card, Descriptions, Button, Typography, Modal, Form, Input } from 'antd';
 import { EditOutlined, DeleteOutlined, FileTextOutlined } from '@ant-design/icons';
 import { Loading } from '../components';
 // import { PublicationFormModal } from './publication-form-modal';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
-import { GetTagByTagIdDocument } from '../__generated__/graphql-types';
+import {
+  GetTagByTagIdDocument,
+  UpdateTagByTagIdDocument
+} from '../__generated__/graphql-types';
+import { useDeleteTag } from '../graphql';
 
 const { Text } = Typography;
 
@@ -19,6 +23,9 @@ export const TagCard: React.FC<TagCardProps>
    = ({ tagId }) => {
 
   const [ isEditing, setIsEditing ] = useState(false);
+  const [ confirmDelete, setConfirmDelete ] = useState(false);
+  const [ updateTagLoading, setUpdateTagLoading ] = useState(false);
+  const [ form ] = Form.useForm();
   const {
     data,
     loading,
@@ -33,6 +40,9 @@ export const TagCard: React.FC<TagCardProps>
     }
   );
 
+  const [ updateTag ] = useMutation(UpdateTagByTagIdDocument);
+  const [ deleteTag ] = useDeleteTag();
+
   if (loading) return <Loading />;
   if (error || !data) return <p>Error in retrieving tag data</p>
 
@@ -41,15 +51,72 @@ export const TagCard: React.FC<TagCardProps>
 
   if (tag == null) return <p>Error in Tag Data</p>;
 
+  const onOk = async () => {
+    setUpdateTagLoading(true);
+    const values = await form.validateFields();
+    console.log('edit tags modal values:\n', values);
+    const updateRes = await updateTag({
+      variables: {
+        tagId: tag.tagId,
+        tagPatch: {
+          tagName: values.tagName
+        }
+      }
+    });
+    console.log('update tag response', updateRes);
+    setUpdateTagLoading(false);
+    setIsEditing(false);
+  }
+
   return (
-    <Card
+    <Fragment>
+      <Modal
+        title="Edit Tag"
+        visible={isEditing}
+        onCancel={() => setIsEditing(false)}
+        onOk={onOk}
+        confirmLoading={updateTagLoading}
+      >
+        <Form
+          layout="vertical"
+          initialValues={{
+            tagName: tag.tagName
+          }}
+          form={form}
+        >
+          <Form.Item label="Tag Name" name="tagName" required={true}
+          rules={[
+            {
+              type: "string",
+              min: 1
+            }
+          ]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal
+        visible={confirmDelete}
+        title="Confirm Delete"
+        okText="Delete"
+        okButtonProps={{danger: true}}
+        onOk={() => {
+          deleteTag({ variables: { tagId: tag.tagId }})
+          setConfirmDelete(false);
+        }}
+        onCancel={() => setConfirmDelete(false)}
+        >
+          Are you sure you want to delete this tag?
+       </Modal>
+      <Card
       title={tag.tagName}
       extra={[
         <Button key="Edit" icon={<EditOutlined />} size="small" shape="circle" type="link"
           onClick={() => setIsEditing(!isEditing)}
         />,
         <Button key="Delete" icon={<DeleteOutlined />} size="small" shape="circle" type="link"
-          // onClick={onDelete}
+          onClick={() => setConfirmDelete(true)}
         />,
       ]}
       // css={{
@@ -67,19 +134,8 @@ export const TagCard: React.FC<TagCardProps>
           <Text strong={true}>Annotations: </Text>View {tag.annotationTagsByTagId.totalCount} <FileTextOutlined/>
         </Link>
       </div>
-
-      {/* <PublicationFormModal
-        visible={isEditing}
-        setVisible={setIsEditing}
-        initialValues={
-          {
-            publicationId,
-            title,
-            authors
-          }
-        }
-      /> */}
     </Card>
+    </Fragment>
   )
 }
 
