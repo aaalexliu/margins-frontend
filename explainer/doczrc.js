a872da67923c7e65ad82eea01136b5cd591f775e
alexliusq <uilxela7@gmail.com>
2020-12-10 22:42:09 +0800

    basic intro set up

--- explainer/doczrc.js
+++ explainer/doczrc.js
@@ -2,8 +2,8 @@ export default {
   // base: './projects/margins-me',
   src: './docs',
   debug: true,
-  repository: 'https://github.com/alexliusq/margins-me-frontend',
-  title: "Margins",
+  // repository: 'https://github.com/alexliusq/margins-me-frontend',
+  title: "Margins: Technical Overview",
   description: "How I Built Margins",
   // link: "ssdsfas"
 }
