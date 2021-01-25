e420b4abe6a09d2e5ab842a92989214a8b9a1f85
alexliusq <uilxela7@gmail.com>
2021-01-26 00:05:02 +0800

    add google analytics

--- margins-me-gatsby/gatsby-config.js
+++ margins-me-gatsby/gatsby-config.js
@@ -39,13 +39,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA-254370736", // Google Analytics / GA
-          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          "G-35MPPFN61D"
         ],
-      },
+        gtagConfig: {
+          anonymize_ip: false
+        },
+        pluginConfig: {
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      }
     },
     {
       resolve: `gatsby-plugin-typescript`,
