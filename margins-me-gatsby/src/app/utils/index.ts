57a97fd1c7d305a2a58715f28e4a62a7e93c49c8
alexliusq <uilxela7@gmail.com>
2021-01-25 14:03:17 +0800

    Revert "first draft of README"
    This reverts commit 0478912c67bafee3845cbfc29b8cf4d6f2e34e38.

---
+++ margins-me-gatsby/src/app/utils/index.ts
@@ -0,0 +1,4 @@
+export { getAccountId } from './account-id';
+export { generateObjectId } from './object-id';
+export { extractAnnotationSections } from './annotation-sections';
+export type { Section } from './annotation-sections';
